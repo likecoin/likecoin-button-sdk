4d8a595db5f36a26c37577e23df78269dfc28990
heloguanyun <guanyun.helo@gmail.com>
2020-07-21 21:18:22 +0800

    fix: 🐛 fix eslint issue

--- src/sdk.esm.js
+++ src/sdk.esm.js
@@ -1,37 +1,40 @@
 
 /**
  * LikeButtonSdk Constructor
- * @config liker info likerId:string ref:HtmlElement | string  href:string
+ * @config liker info likerId:string ref:HtmlElement | string  href?:string
  */
 
 class LikeCoinButton {
-    constructor(config) {
-        this.likerId = config.likerId
-        this.ref = config.ref
-        this.href = config.href ? config.href : window.location.href
-        this.insertStyle()
-    }
-    mount() {
-        if (this.ref instanceof HTMLElement === false) {
-            this.ref = document.querySelector(`${this.ref}`)
-        }
-        // set like user info
-        this.ref.classList.add('likecoin-embed', 'likecoin-button')
-        this.ref.setAttribute('data-liker-id', this.likerId)
-        this.ref.setAttribute('data-href', this.href)
+  constructor(config) {
+    this.likerId = config.likerId;
+    this.ref = config.ref;
+    this.href = config.href ? config.href : window.location.href;
+    this.likeStyleElement = null;
+    this.insertStyle();
+  }
 
-        this.href = encodeURIComponent(this.href);
-        const src = `https://button.like.co/in/embed/${this.likerId}/button?referrer=${this.href}`;
-        this.ref.appendChild(document.createElement('div'));
-        const iframe = document.createElement('iframe');
-        iframe.setAttribute('src', src);
-        iframe.setAttribute('frameborder', 0);
-        iframe.setAttribute('scrolling', 'no');
-        this.ref.appendChild(iframe);
+  mount() {
+    if (this.ref instanceof HTMLElement === false) {
+      this.ref = document.querySelector(`${this.ref}`);
     }
-    insertStyle() {
-        const style = document.createElement('style');
-        style.innerHTML = `
+    // set like user info
+    this.ref.classList.add('likecoin-embed', 'likecoin-button');
+    this.ref.setAttribute('data-liker-id', this.likerId);
+    this.ref.setAttribute('data-href', this.href);
+
+    this.href = encodeURIComponent(this.href);
+    const src = `https://button.like.co/in/embed/${this.likerId}/button?referrer=${this.href}`;
+    this.ref.appendChild(document.createElement('div'));
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('src', src);
+    iframe.setAttribute('frameborder', 0);
+    iframe.setAttribute('scrolling', 'no');
+    this.ref.appendChild(iframe);
+  }
+
+  insertStyle() {
+    this.likeStyleElement = document.createElement('style');
+    this.likeStyleElement.innerHTML = `
             .likecoin-button {
             position: relative;
             width: 100%;
@@ -50,8 +53,8 @@ class LikeCoinButton {
             height: 100%;
             }
         `;
-        document.body.appendChild(style);
-    }
+    document.body.appendChild(this.likeStyleElement);
+  }
 }
 
-export default LikeCoinButton
+export default LikeCoinButton;
